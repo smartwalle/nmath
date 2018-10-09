0a23bfd41d1d2734672849c197dd98a265d2ffcc
SmartWalle <smartwalle@gmail.com>
2018-10-09 16:05:32 +0800

    Floor 和 Ceil 支持负数

--- floor.go
+++ floor.go
@@ -13,11 +13,13 @@ func Round(f float64, n int) float64 {
 // Floor 保留 n 位小数，并对小数部分进行向下取整
 func Floor(f float64, n int) float64 {
 	pow10n := math.Pow10(n)
+	if f < 0 {
+		return (math.Trunc((f*-1+0.9/pow10n)*pow10n) / pow10n) * -1
+	}
 	return math.Trunc(f*pow10n) / pow10n
 }
 
 // Ceil 保留 n 位小数，并对小数部分进行向上取整
 func Ceil(f float64, n int) float64 {
-	pow10n := math.Pow10(n)
-	return math.Trunc((f+0.9/pow10n)*pow10n) / pow10n
+	return Floor(f*-1, n) * -1
 }
--- floor_test.go
+++ floor_test.go
@@ -2,10 +2,12 @@ package math4go
 
 import (
 	"fmt"
+	"math"
 	"testing"
 )
 
 func TestRound(t *testing.T) {
+	fmt.Println("===== Round =====")
 	var v = 10.0666
 	fmt.Println(v, Round(v, 2))
 	if Round(v, 2) != 10.07 {
@@ -20,6 +22,7 @@ func TestRound(t *testing.T) {
 }
 
 func TestFloor(t *testing.T) {
+	fmt.Println("===== Floor =====")
 	var v = 10.999
 	fmt.Println(v, Floor(v, 2))
 	if Floor(v, 2) != 10.99 {
@@ -37,9 +40,13 @@ func TestFloor(t *testing.T) {
 	if Floor(v, 2) != 10.91 {
 		t.FailNow()
 	}
+
+	fmt.Println(math.Floor(-10.1), Floor(-10.111, 2))
+	fmt.Println(math.Floor(10.1), Floor(10.111, 2))
 }
 
 func TestCeil(t *testing.T) {
+	fmt.Println("===== Ceil =====")
 	var v = 10.119
 	fmt.Println(v, Ceil(v, 2))
 	if Ceil(v, 2) != 10.12 {
@@ -57,4 +64,7 @@ func TestCeil(t *testing.T) {
 	if Ceil(v, 2) != 10.11 {
 		t.FailNow()
 	}
+
+	fmt.Println(math.Ceil(-10.1), Ceil(-10.121, 2))
+	fmt.Println(math.Ceil(10.1), Ceil(10.121, 2))
 }
