8106ce995f3fb82edac2fc3055cd1326a18c5d29
smartwalle <smartwalle@gmail.com>
2020-07-10 10:24:39 +0800

    优化

---
+++ int.go
@@ -0,0 +1,43 @@
+package math4go
+
+func Max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func MaxInt32(x, y int32) int32 {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func MaxInt64(x, y int64) int64 {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func Min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+func MinInt32(x, y int32) int32 {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+func MinInt64(x, y int64) int64 {
+	if x < y {
+		return x
+	}
+	return y
+}
