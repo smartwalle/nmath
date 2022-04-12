f66c37bce389c135b3599546888f5d9e4763f5d7
smartwalle <smartwalle@gmail.com>
2022-04-12 10:34:40 +0800

    泛型

--- number.go
+++ number.go
@@ -1,15 +1,17 @@
 package math4go
 
-import "math"
+import (
+	"math"
+)
 
-type Comparable interface {
+type Number interface {
 	int | int8 | int16 | int32 | int64 | uint | uint8 | uint16 | uint32 | uint64 | float32 | float64
 }
 
-func Max[T Comparable](x, y T) T {
+func Max[T Number](x, y T) T {
 	return T(math.Max(float64(x), float64(y)))
 }
 
-func Min[T Comparable](x, y T) T {
+func Min[T Number](x, y T) T {
 	return T(math.Min(float64(x), float64(y)))
 }
