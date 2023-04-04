eb7846daef381ea008438e269c3f2a7436f09060
smartwalle <smartwalle@gmail.com>
2023-04-04 17:15:09 +0800

    Number 调整

--- number.go
+++ number.go
@@ -5,7 +5,7 @@ import (
 )
 
 type Number interface {
-	int | int8 | int16 | int32 | int64 | uint | uint8 | uint16 | uint32 | uint64 | float32 | float64
+	~int | ~int8 | ~int16 | ~int32 | ~int64 | ~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~float32 | ~float64
 }
 
 func Max[T Number](x, y T) T {
